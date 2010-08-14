8a588bb05f5168e3b177470eacc48b27e9d4afa1
Jason Simeone <jay@classless.net>
2010-08-14 18:36:11 -0400

    Forgot to change the __all__ when item class changed.

--- memcacheinspector/__init__.py
+++ memcacheinspector/__init__.py
@@ -5,7 +5,7 @@ import socket
 import memcache
 
 
-__all__ = ('MemcacheInspector', 'MemcacheKeyInfo', 'MemcacheInspectorError')
+__all__ = ('MemcacheInspector', 'MemcacheItem', 'MemcacheInspectorError')
 __version_info__ = (0, 1, 0)
 __version__ = '.'.join([str(x) for x in __version_info__])
 
